f0ba931f53bf3a0a0428ec18b0edb179c05a51a9
Samuele Sacchetti <sacchettisamuele@gmail.com>
2024-06-07 12:00:02 +0200

    - bottom navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
         useMaterial3: true,
       ),
-      home: const DashboardFragment(),
+      home: const ProfileFragment(),
     );
   }
 }
